04b67e18c552e372c0c4c216a3097c8d3731b277
lakrouehamza <lakrounehamza10@gmail.com>
2024-12-18 10:52:49 +0100

    ajoute page home de client

--- views/client/home.php
+++ views/client/home.php
@@ -34,5 +34,316 @@
         </nav>
     </div>
 </header>
-</body>
+
+<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div
+        class="relative isolate overflow-hidden bg-white px-6 py-20 text-center sm:px-16 sm:shadow-sm">
+        <p class="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        Find your lawyer. Connect online.
+        </p>
+
+
+        <form action="/search">
+            <label
+                class="mx-auto mt-8 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300"
+                for="search-bar">
+
+                <input id="search-bar" placeholder="your keyword here" name="q"
+                    class="px-6 py-2 w-full rounded-md flex-1 outline-none bg-white" required="">
+                <button type="submit"
+                    class="w-full md:w-auto px-6 py-3 bg-black border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all">
+                    <div class="flex items-center transition-all opacity-1">
+                        <span class="text-sm font-semibold whitespace-nowrap truncate mx-auto">
+                            Search
+                        </span>
+                    </div>
+                </button>
+            </label>
+        </form>
+
+        <svg viewBox="0 0 1024 1024"
+            class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
+            aria-hidden="true">
+            <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7">
+            </circle>
+            <defs>
+                <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+                    <stop stop-color="#3b82f6"></stop>
+                    <stop offset="1" stop-color="#1d4ed8"></stop>
+                </radialGradient>
+            </defs>
+        </svg>
+    </div>
+</div>
+
+<p class="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        Find your lawyer. Connect online.
+</p>
+<div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+    
+    <div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
+        <div class="rounded overflow-hidden shadow-lg">
+
+            <a href="#"></a>
+            <div class="relative">
+                <a href="#">
+                    <img class="w-full"
+                        src="https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+                        alt="Sunset in the mountains">
+                    <div
+                        class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                    </div>
+                </a>
+                <a href="#!">
+                    <div
+                        class="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        Photos
+                    </div>
+                </a>
+
+                <a href="!#">
+                    <div
+                        class="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        <span class="font-bold">27</span>
+                        <small>March</small>
+                    </div>
+                </a>
+            </div>
+            <div class="px-6 py-4">
+
+                <a href="#"
+                    class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                    View in Newyork City</a>
+                <p class="text-gray-500 text-sm">
+                    The city that never sleeps
+                </p>
+            </div>
+            <div class="px-6 py-4 flex flex-row items-center">
+                <span href="#" class="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row items-center">
+                    <svg height="13px" width="13px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+                        style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                        <g>
+                            <g>
+                                <path d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256
+			c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128
+			c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z"></path>
+                            </g>
+                        </g>
+                    </svg>
+                    <span class="ml-1">6 mins ago</span></span>
+            </div>
+        </div>
+        <div class="rounded overflow-hidden shadow-lg">
+            <a href="#"></a>
+            <div class="relative">
+                <a href="#">
+                    <img class="w-full"
+                        src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+                        alt="Sunset in the mountains">
+                    <div
+                        class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                    </div>
+                </a>
+                <a href="#!">
+                    <div
+                        class="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        Photos
+                    </div>
+                </a>
+
+                <a href="!#">
+                    <div
+                        class="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        <span class="font-bold">20</span>
+                        <small>March</small>
+                    </div>
+                </a>
+            </div>
+            <div class="px-6 py-4">
+
+                <a href="#"
+                    class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                    Pizza in Town</a>
+                <p class="text-gray-500 text-sm">
+                    The collection of best pizza images in Newyork city
+                </p>
+            </div>
+            <div class="px-6 py-4 flex flex-row items-center">
+                <span href="#"
+                    class="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row justify-between items-center">
+                    <svg height="13px" width="13px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+                        style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                        <g>
+                            <g>
+                                <path
+                                    d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256 c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128 c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z">
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                    <span class="ml-1">3 mins read</span></span>
+            </div>
+        </div>
+        <div class="rounded overflow-hidden shadow-lg">
+
+            <a href="#"></a>
+            <div class="relative">
+                <a href="#">
+                    <img class="w-full"
+                        src="https://images.pexels.com/photos/257816/pexels-photo-257816.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+                        alt="Sunset in the mountains">
+                    <div
+                        class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                    </div>
+                </a>
+                <a href="#!">
+                    <div
+                        class="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        Photos
+                    </div>
+                </a>
+
+                <a href="!#">
+                    <div
+                        class="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        <span class="font-bold">15</span>
+                        <small>April</small>
+                    </div>
+                </a>
+            </div>
+            <div class="px-6 py-4">
+
+                <a href="#"
+                    class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                    Salad Images ever</a>
+                <p class="text-gray-500 text-sm">
+                    The collection of best salads of town in pictures
+                </p>
+            </div>
+            <div class="px-6 py-4 flex flex-row items-center">
+                <span href="#"
+                    class="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row justify-between items-center">
+                    <svg height="13px" width="13px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+                        style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                        <g>
+                            <g>
+                                <path
+                                    d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256 c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128 c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z">
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                    <span class="ml-1">6 mins read</span></span>
+            </div>
+        </div>
+    </div>
+</div>
+<p class="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        Find Lawyers with 5 Star Verified Reviews.
+</p>
+<div class="container mx-auto py-48">
+        <div class="grid lge:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 mdsm:grid-cols-2 sm:grid-cols-1 lg:gap-6 lge:gap-6 md:gap-16 mdsm:gap-16 sm:gap-16 lg:px-2 lge:px-2 md:px-2 mdsm:px-2 sm:px-2">
+  
+          <div class="group relative">
+            <div class="-top-12 left-[34%] absolute">
+                <img 
+           src="https://makecomponents.com/Image/teams/Team001/person1.webp"
+            class="rounded-full transition delay-200 group-hover:border-[#bde9ff] border-4 border-[#001f2e] object-cover h-[100px] w-[100px]"
+             alt="Michael Thompson" /> </div>
+            <div class="group-hover:bg-[#001f2e] transition delay-200 bg-[#bde9ff] pt-16 px-4 pb-4">
+              <h2 class=" group-hover:text-white transition delay-200 text-[#001f2e] text-center text-2xl font-semibold">
+                David Nguyen
+              </h2>
+              <p class="group-hover:text-white transition delay-200 pb-10 text-[#001f2e] pt-4 leading-6 text-center text-base">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries for previewing layouts and visual
+                mockups
+              </p>
+            </div>
+          </div>
+  
+          <div class="group relative">
+            <div class="-top-12 left-[34%] absolute">
+              <img 
+           src="https://makecomponents.com/Image/teams/Team001/person2.webp"
+            class="rounded-full transition delay-200 group-hover:border-[#bde9ff] border-4 border-[#001f2e] object-cover h-[100px] w-[100px]"
+             alt="Michael Thompson" />
+            </div>
+            <div class="group-hover:bg-[#001f2e] transition delay-200 bg-[#bde9ff] pt-16 px-4 pb-4">
+              <h2 class=" group-hover:text-white transition delay-200 text-[#001f2e] text-center text-2xl font-semibold">
+                Michael Thompson
+              </h2>
+              <p class="group-hover:text-white transition delay-200 pb-10 text-[#001f2e] pt-4 leading-6 text-center text-base">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries for previewing layouts and visual
+                mockups
+              </p>
+            </div>
+          </div>
+  
+          <div class="group relative">
+            <div class="-top-12 left-[34%] absolute">
+              <img   src="https://makecomponents.com/Image/teams/Team001/person3.webp" class="rounded-full transition delay-200 group-hover:border-[#bde9ff] border-4 border-[#001f2e] object-cover h-[100px] w-[100px]" alt="Emily Parker"/>
+            </div>
+            <div class="group-hover:bg-[#001f2e] transition delay-200 bg-[#bde9ff] pt-16 px-4 pb-4">
+              <h2 class=" group-hover:text-white transition delay-200 text-[#001f2e] text-center text-2xl font-semibold">
+                Emily Parker
+              </h2>
+              <p class="group-hover:text-white transition delay-200 pb-10 text-[#001f2e] pt-4 leading-6 text-center text-base">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries for previewing layouts and visual
+                mockups
+              </p>
+            </div>
+          </div>
+  
+          <div class="group relative">
+            <div class="-top-12 left-[34%] absolute">
+              <img   src="https://makecomponents.com/Image/teams/Team001/person4.webp"
+              alt=" James Anderson" class="rounded-full transition delay-200 group-hover:border-[#bde9ff] border-4 border-[#001f2e] object-cover h-[100px] w-[100px]"/>
+            </div>
+            <div class="group-hover:bg-[#001f2e] transition delay-200 bg-[#bde9ff] pt-16 px-4 pb-4">
+              <h2 class=" group-hover:text-white transition delay-200 text-[#001f2e] text-center text-2xl font-semibold">
+                James Anderson
+              </h2>
+              <p class="group-hover:text-white transition delay-200 pb-10 text-[#001f2e] pt-4 leading-6 text-center text-base">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries for previewing layouts and visual
+                mockups
+              </p>
+            </div>
+          </div>
+  
+        </div>
+      </div>
+<footer>
+<div class="bg-gray-100">
+    <div class="max-w-screen-xl py-10 px-4 sm:px-6 text-gray-800 sm:flex justify-between mx-auto">
+        <div class="p-5 sm:w-8/12">
+            <h3 class="font-bold text-3xl text-indigo-600 mb-4">Componentity</h3>
+
+            <div class="flex text-gray-500 uppercase text-sm">
+                <a href="#" class="mr-2 hover:text-indigo-600">Home</a>
+                <a href="#" class="mr-2 hover:text-indigo-600">About Us</a>
+                <a href="#" class="mr-2 hover:text-indigo-600">Contact Us</a>
+                <a href="#" class="mr-2 hover:text-indigo-600">Support Us</a>
+            </div>
+
+        </div>
+        <div class="p-5 sm:w-4/12">
+            <h3 class="font-medium text-lg text-indigo-600 mb-4">Subscribe to our Newsletter</h3>
+            <form class="mt-4">
+                <input class="border rounded w-full px-4 py-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="username@email.com">
+            </form>
+        </div>
+    </div>
+    <div class="flex py-5 m-auto text-gray-800 text-sm flex-col items-center border-t max-w-screen-xl">
+        <p>© Copyright 2020. All Rights Reserved.</p>
+    </div>
+</div>
+</footer>
+    </body>
 </html>
---
+++ views/page404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<div class="bg-gray-100">
+    <div class="h-screen flex flex-col justify-center items-center">
+        <h1 class="text-8xl font-bold text-gray-800">404</h1>
+        <p class="text-4xl font-medium text-gray-800">Page Not Found</p>
+        <!-- <a href="/" class="mt-4 text-xl text-blue-600 hover:underline">Go back home</a> -->
+    </div>
+</div>  
+</body>
+</html>
